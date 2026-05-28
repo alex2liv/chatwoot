e2dc196f38565acd6a08603e780a8508085eca21
alex2liv <alex@metasyncdigital.com.br>
2026-05-28 14:25:01 -0300

    fix: add missing onInternalChat handlers to ActionCable connector

--- app/javascript/dashboard/helper/actionCable.js
+++ app/javascript/dashboard/helper/actionCable.js
@@ -214,6 +214,91 @@ class ActionCableConnector extends BaseActionCableConnector {
     this.app.$store.dispatch('inboxes/revalidate', { newKey: keys.inbox });
     this.app.$store.dispatch('teams/revalidate', { newKey: keys.team });
   };
+
+  onInternalChatMessageCreated = data => {
+    this.app.$store.dispatch('internalChat/messages/addMessageFromCable', {
+      channelId: data.internal_chat_channel_id,
+      message: data,
+    });
+    const channel = this.app.$store.getters['internalChat/getChannelById'](data.internal_chat_channel_id);
+    if (channel) {
+      const currentUserId = this.app.$store.getters.getCurrentUser?.id;
+      const isOwnMessage = data.sender?.id === currentUserId;
+      const activeChannelId = this.app.$store.getters['internalChat/getActiveChannelId'];
+      const isActiveChannel = activeChannelId === data.internal_chat_channel_id;
+      const mentionedIds = data.content_attributes?.mentioned_user_ids || [];
+      const isMentioned = mentionedIds.includes(currentUserId);
+      this.app.$store.dispatch('internalChat/updateChannel', {
+        id: data.internal_chat_channel_id,
+        unread_count: isActiveChannel || isOwnMessage ? channel.unread_count || 0 : (channel.unread_count || 0) + 1,
+        has_unread_mention: isActiveChannel || isOwnMessage ? false : channel.has_unread_mention || isMentioned,
+        last_activity_at: data.created_at,
+      });
+    }
+  };
+
+  onInternalChatMessageUpdated = data => {
+    this.app.$store.dispatch('internalChat/messages/updateMessageFromCable', {
+      channelId: data.internal_chat_channel_id,
+      message: data,
+    });
+  };
+
+  onInternalChatMessageDeleted = data => {
+    this.app.$store.dispatch('internalChat/messages/deleteMessageFromCable', {
+      channelId: data.internal_chat_channel_id,
+      messageId: data.id,
+    });
+  };
+
+  onInternalChatTypingOn = ({ channel, user }) => {
+    this.app.$store.dispatch('internalChatTypingStatus/create', { channelId: channel.id, user });
+  };
+
+  onInternalChatTypingOff = ({ channel, user }) => {
+    this.app.$store.dispatch('internalChatTypingStatus/destroy', { channelId: channel.id, user });
+  };
+
+  onInternalChatReactionCreated = data => {
+    this.app.$store.dispatch('internalChat/messages/addReactionFromCable', {
+      channelId: data.internal_chat_channel_id,
+      messageId: data.message_id,
+      reaction: data,
+    });
+  };
+
+  onInternalChatReactionDeleted = data => {
+    this.app.$store.dispatch('internalChat/messages/removeReactionFromCable', {
+      channelId: data.internal_chat_channel_id,
+      messageId: data.message_id,
+      reactionId: data.id,
+    });
+  };
+
+  onInternalChatChannelUpdated = data => {
+    const currentUserId = this.app.$store.getters.getCurrentUser?.id;
+    const memberIds = data.member_user_ids;
+    if (memberIds && currentUserId && data.channel_type === 'private_channel') {
+      if (!memberIds.includes(currentUserId)) {
+        this.app.$store.commit('internalChat/DELETE_CHANNEL', data.id);
+        return;
+      }
+      const existing = this.app.$store.getters['internalChat/getChannelById'](data.id);
+      if (!existing) {
+        this.app.$store.dispatch('internalChat/get');
+        return;
+      }
+    }
+    this.app.$store.dispatch('internalChat/updateChannel', data);
+  };
+
+  onInternalChatPollVoted = data => {
+    this.app.$store.dispatch('internalChat/polls/updatePollFromCable', {
+      channelId: data.internal_chat_channel_id,
+      pollId: data.poll_id,
+      option: data,
+    });
+  };
 }
 
 export default {
